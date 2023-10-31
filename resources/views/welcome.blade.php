6fb65882021d331dcf282241a80fce05a26498b1
Antonio Altamura <antoalta95@outlook.it>
2023-10-31 17:29:42 +0100

    modifica parziale home e navba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,53 @@
 <x-layout>
+
+
+    {{-- TITOLO --}}
+    <div class="container m-5 justify-content-center d-flex">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center display-1">PROVA</h1>
+            </div>
+        </div>
+    </div>
     
+    {{-- INSERIMENTO CARD --}}
+    <div class="container justify-content-center d-flex">
+        <div class="row m-5 ">
+            <div class="col-12 col-md-4">
+                <div class="card card " style="width: 18rem;">
+                    <img src="https://picsum.photos/200" class="card-img-top" alt="immagini">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                    
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card card " style="width: 18rem;">
+                    <img src="https://picsum.photos/200" class="card-img-top" alt="immagini">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                    
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card card " style="width: 18rem;">
+                    <img src="https://picsum.photos/200" class="card-img-top" alt="immagini">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                    
+            </div>
+        </div>
+    </div>
+   
 </x-layout>
+
