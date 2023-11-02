00fd0b4028e17366925def3cd203e0311e1e0b62
Michele Favia <michelefavia80gmail.com>
2023-11-02 16:28:36 +0100

    modifica navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,23 @@
 import './bootstrap';
 import 'bootstrap/dist/js/bootstrap.js';
+
+
+
+//USARE L'OGGETTO "SCROLL"
+
+let navbar= document.querySelector("#navbar")
+
+
+window.addEventListener("scroll",()=>{
+    // console.log(window.scrollY);
+
+
+    if (window.scrollY > 0) {
+
+        navbar.classList.add("navbar-scrolled")
+
+    } else{
+        navbar.classList.remove("navbar-scrolled")
+    }
+}
+)
