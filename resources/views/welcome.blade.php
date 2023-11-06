e293d285c95529dc3ea440532e56e778f26966a6
OnofrioSoranno <sorannoonofrio@outlook.it>
2023-11-06 10:39:28 +0100

    demo 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,16 +39,16 @@
 
     {{-- INSERIMENTO CARD --}}
     
-    <div class="container justify-content-center d-flex mt-5">
+    <div class="container justify-content-center d-flex mt-5 ">
       <div class="row m-5 ">
           @foreach ($articles as $article)
             <div class="col-12 col-md-4">
-                <div class="card card " style="width: 18rem;">
+                <div class="card card mb-5 " style="width: 18rem;">
                     <img src="https://picsum.photos/200" class="card-img-top" alt="immagini">
                     <div class="card-body">
                       <h5 class="card-title">{{$article->titolo}}</h5>
                       <p class="card-text">Categoria: {{$article->category->name}}</p>
-                      <p class="card-text">{{$article->prezzo}}</p>
+                      <p class="card-text">€ {{$article->prezzo}}</p>
                       <p class="card-text">{{$article->descrizione}}</p>
                       <a href="{{route('showArticle', compact('article'))}}" class="btn btn-primary">Visualizza</a>
                     </div>
