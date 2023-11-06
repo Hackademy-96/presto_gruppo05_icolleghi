2ba068b820300cc34dfd646057afff150da84e4d
OnofrioSoranno <sorannoonofrio@outlook.it>
2023-11-06 18:03:21 +0100

    revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    {{$article_to_check ? "Ecco l'annuncio da revisionare" : 'Non ci sono annunci da revisionare'}}
+                </h1>
+            </div>
+        </div>
+    </div>
+    @if($article)
+
 </x-layout>
